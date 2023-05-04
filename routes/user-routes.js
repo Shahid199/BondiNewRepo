52ba87fb868a8e74d32dc888330bee42855cf6eb
Mominul Haque <sejan840@gmail.com>
2023-05-04 19:51:33 +0600

    role authorization and cors enabled

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require('passport');
 const {
   createOfficeUser,
   createStudentUser,
@@ -7,13 +8,14 @@ const {
   loginSuperAdmin
   // createSuperAdmin
 } = require("../controller/user-controller");
+const authorize = require('../utilities/authorizationMiddleware');
 const router = express.Router();
 
-router.post("/createofficeuser", createOfficeUser);
-router.post("/createstudentuser", createStudentUser);
-router.get("/getuserbyrole/:role", getUserByRole);
-router.get("/getuserrole", getUserRole);
-router.post("/login",loginSuperAdmin);
+router.post("/createofficeuser", [passport.authenticate('jwt', { session: false }), authorize(['admin'])], createOfficeUser);
+router.post("/createstudentuser", [passport.authenticate('jwt', { session: false }), authorize(['superadmin', 'admin'])], createStudentUser);
+router.get("/getuserbyrole/:role", [passport.authenticate('jwt', { session: false }), authorize(['superadmin', 'admin'])], getUserByRole);
+router.get("/getuserrole", [passport.authenticate('jwt', { session: false }), authorize(['admin'])], getUserRole);
+router.post("/login", loginSuperAdmin);
 
 // router.get('/create_super_admin',createSuperAdmin);
 
