5ff812fc9e857f3883fb4b70351cb222d927230d
Mominul Haque <sejan840@gmail.com>
2023-05-27 23:52:25 +0600

    minor adjustment

--- controller/student-controller.js
+++ controller/student-controller.js
@@ -647,9 +647,8 @@ const missedExam = async (req, res, next) => {
 
     resultFinal.push(result);
   }
-  if (resultFinal.length == 0)
-    return res.status(200).json("No Missed Exam Available.");
-  else return res.status(200).json(resultFinal);
+ 
+  return res.status(200).json(resultFinal);
 };
 const retakeExam = async (req, res, next) => {
   const ObjectId = mongoose.Types.ObjectId;
