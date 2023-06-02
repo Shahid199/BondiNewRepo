883238bccf2be50367ebedbdc8d82cb12dde0973
Shahid199 <uiu.shahid.imran@gmail.com>
2023-06-02 19:27:26 +0600

    missed ,history admin.....

--- controller/student-controller.js
+++ controller/student-controller.js
@@ -894,8 +894,8 @@ const missedExam = async (req, res, next) => {
     result["subject"] = resultData[i].subjectId.name;
     result["startTime"] = moment(resultData[i].startTime).format("LL");
     result["duration"] = Number(resultData[i].duration);
-    result["examType"] = examType(Number(resultData[i].examType));
-    result["examVariation"] = examVariation(Number(resultData[i].examVariation));
+    result["examType"] = examType[Number(resultData[i].examType)];
+    result["examVariation"] = examVariation[Number(resultData[i].examVariation)];
     result["negativeMarks"] = resultData[i].negativeMarks;
     resultFinal.push(result);
   }
