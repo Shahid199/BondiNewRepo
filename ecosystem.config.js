a12f67393629ba35b8ec152b006ab2141ae2556b
Shahid199 <uiu.shahid.imran@gmail.com>
2023-06-09 01:23:00 +0600

    pm2 setting change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    apps : [
-  {	
-      name:'bondipathshala',
-      instances:'max',
-      exec_mode:'cluster',
-      script: 'app.js',
-      watch: '.',
-      ignore_watch : ["node_modules", "uploads"]
-    }]
-  }
-  
+  apps: [
+    {
+      name: "bondipathshala",
+      instances: "max",
+      exec_mode: "cluster",
+      script: "app.js",
+    },
+  ],
+};
