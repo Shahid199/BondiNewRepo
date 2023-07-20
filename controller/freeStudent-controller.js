ae28809550f354ef2faa8d29e7768df8948ad57a
Shahid199 <uiu.shahid.imran@gmail.com>
2023-07-21 00:55:47 +0600

    free update 6

--- controller/freeStudent-controller.js
+++ controller/freeStudent-controller.js
@@ -814,7 +814,7 @@ const submitAnswerFree = async (req, res, next) => {
   data1["studExamEndTime"] = moment(dataTime.examEndTime).format("LLL");
   data1["studExamTime"] = dataTime.duration;
   data1["marksPerMcq"] = getResult.examId.marksPerMcq;
-  data1["marksPerWrong"] = getresult.examId.negativeMarks / 100;
+  data1["marksPerWrong"] = getResult.examId.negativeMarks / 100;
   return res.status(200).json(data1);
 };
 //error handle and ranks update
