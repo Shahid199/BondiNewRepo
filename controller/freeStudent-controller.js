d80dc401c01d0a894e63170b4efb57c28a561bc2
Shahid199 <uiu.shahid.imran@gmail.com>
2023-08-10 23:58:48 +0600

    new commit written

--- controller/freeStudent-controller.js
+++ controller/freeStudent-controller.js
@@ -380,8 +380,8 @@ const getFreeExamId = async (req, res, next) => {
       $and: [
         { status: true },
         { examFreeOrNot: true },
-        { startTime: { $lt: "2023-08-10T11:56:00.000Z" } },
-        { endTime: { $gt: "2023-08-10T11:56:00.000Z" } },
+        { startTime: { $lt: currentTime } },
+        { endTime: { $gt: currentTime } },
       ],
     });
   } catch (err) {
