4df9a62afcdf54a2b8273feb63add5e068868f23
Shahid199 <uiu.shahid.imran@gmail.com>
2023-08-12 01:25:19 +0600

    new commit written

--- controller/freeStudent-controller.js
+++ controller/freeStudent-controller.js
@@ -1415,7 +1415,7 @@ const updateNullData = async (req, res, next) => {
   let students;
   try {
     students = await FreestudentMarksRank.find({
-      $and: [{ examId: examId }, { totalObtainedMarks: null }],
+      $and: [{ examId: examId }],
     }).select("studentId -_id");
   } catch (err) {
     return res.status(200).json("Soomething went wrong.");
