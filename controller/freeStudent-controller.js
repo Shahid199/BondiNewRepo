9f23ba0d0a084416766ddb548c3979f43aa5bf13
Shahid199 <uiu.shahid.imran@gmail.com>
2023-08-12 04:21:33 +0600

    new commit written

--- controller/freeStudent-controller.js
+++ controller/freeStudent-controller.js
@@ -1396,7 +1396,7 @@ const getAllRankFree = async (req, res, next) => {
   // }
   let allData = [];
   let totalStudent = null;
-  for (let i = 0; i < resultRank.length; i++) {
+  for (let i = 0; i < resultRank.length - 1; i++) {
     let data1 = {};
     let conData = "*******";
     data1["examName"] = resultRank[i].examId.name;
