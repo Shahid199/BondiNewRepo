4f4a6597daf15299fab6598fdf4838a9b00e4ea2
Shahid199 <uiu.shahid.imran@gmail.com>
2023-08-12 03:03:01 +0600

    new commit written

--- controller/freeStudent-controller.js
+++ controller/freeStudent-controller.js
@@ -1384,7 +1384,7 @@ const getAllRankFree = async (req, res, next) => {
   try {
     data2 = await FreestudentMarksRank.find({
       studentId: { $in: freeStudentArr },
-    });
+    }).sort({ totalObtainedMarks: -1 });
   } catch (err) {
     return res.status(500).json("Soomething went wrong.");
   }
