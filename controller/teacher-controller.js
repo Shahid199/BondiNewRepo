03c22506bb99cd5140f419b2e1bc47488c5d5a78
Shahid199 <uiu.shahid.imran@gmail.com>
2023-09-02 05:41:16 +0600

    new written update

--- controller/teacher-controller.js
+++ controller/teacher-controller.js
@@ -30,97 +30,6 @@ const getStudentData = async (req, res, next) => {
   }
   return res.status(200).json(data);
 };
-
-const checkScriptSingle1 = async (req, res, next) => {
-  console.log(req.body);
-  let questionNo = Number(req.body.questionNo);
-  let obtainedMarks = Number(req.body.obtainedMarks);
-  let studentId = req.body.studentId;
-  let examId = req.body.examId;
-  let images = req.body.uploadImages;
-  if (
-    !ObjectId.isValid(studentId) ||
-    !ObjectId.isValid(examId) ||
-    !questionNo ||
-    !obtainedMarks
-  ) {
-    return res
-      .status(404)
-      .json("Student Id or Exam Id or question Id is not valid.");
-  }
-  questionNo = questionNo - 1;
-  // //file upload handle:start
-  // const file = req.files;
-  // //console.log(file);
-  // let questionILinkPath = [];
-  // if (!file.questionILink) return res.status(400).json("Files not uploaded.");
-  // for (let i = 0; i < file.questionILink.length; i++) {
-  //   questionILinkPath[i] = "uploads/".concat(file.questionILink[i].filename);
-  // }
-
-  let uploadImages = [];
-  for (let i = 0; i < images.length; i++) {
-    const matches = String(images[i]).replace(
-      /^data:([A-Za-z-+/]+);base64,/,
-      ""
-    );
-    //const response = {};
-
-    //   var optionalObj = {
-    //     fileName: String(questionNo + 1) + "-" + String(i),
-    //     type: "png",
-    //   };
-    //   console.log(dir);
-    //   uploadImages[i] = base64Img(String(images[i]), dir);
-    // }
-
-    // response.type = matches[1];
-    // response.data = Buffer.from(matches[2], "base64");
-    // let decodedImg = response;
-    // let imageBuffer = decodedImg.data;
-    // let type = decodedImg.type;
-    //let extension = mime.getExtension(type);
-    // //let extension = type;
-    let fileName = "image.png";
-    try {
-      fs.writeFileSync(dir + fileName, matches, { encoding: "base64" });
-      return res.send({ status: "success" });
-    } catch (e) {
-      console.log("nothing");
-      return res.status(500).json(e);
-    }
-  }
-  // console.log(uploadImages);
-  // return res.status(200).json(file);
-  // let studentIdObj = new mongoose.Types.ObjectId(studentId);
-  // let examIdObj = new mongoose.Types.ObjectId(examId);
-  // let getData = null;
-  // try {
-  //   getData = await StudentExamVsQuestionsWritten.findOne({
-  //     $and: [{ studentId: studentIdObj }, { examId: examIdObj }],
-  //   });
-  // } catch (err) {
-  //   return res.status(500).json("Something went wrong.");
-  // }
-  // let insertId = getData._id;
-  // let checkScript = [];
-  // let obtainedMarksArr = [];
-  // checkScript[questionNo] = questionILinkPath;
-  // obtainedMarksArr[questionNo] = obtainedMarks;
-
-  // let upd = {
-  //   ansewerScriptILink: checkScript,
-  //   obtainedMarks: obtainedMarksArr,
-  // };
-  // let doc;
-  // try {
-  //   doc = await StudentExamVsQuestionsWritten.findByIdAndUpdate(insertId, upd);
-  // } catch (err) {
-  //   //console.log(err);
-  //   return res.status(500).json("Something went wrong!");
-  // }
-  // return res.status(201).json("Updated Successfully.");
-};
 const checkScriptSingle = async (req, res, next) => {
   console.log(req.body);
   let questionNo = Number(req.body.questionNo);
@@ -145,7 +54,7 @@ const checkScriptSingle = async (req, res, next) => {
       /^data:([A-Za-z-+/]+);base64,/,
       ""
     );
-    let fileName = "/" + String(questionNo + 1) + "-" + String(i + 1) + ".png";
+    let fileName = String(questionNo + 1) + "-" + String(i + 1) + ".png";
     try {
       fs.writeFileSync(dir + fileName, matches, { encoding: "base64" });
     } catch (e) {
