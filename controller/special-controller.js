7d62bc08ec0f6fc5a35b5212d1fc80e7cec65180
Shahid199 <uiu.shahid.imran@gmail.com>
2023-09-22 12:01:19 +0600

    both updated backend

--- controller/special-controller.js
+++ controller/special-controller.js
@@ -103,7 +103,6 @@ const createSpecialExam = async (req, res, next) => {
     endTime,
     mcqDuration,
     marksPerMcq,
-    negativeVarks,
     totalQuestionMcq,
     totalQuestionWritten,
     writtenDuration,
@@ -121,7 +120,8 @@ const createSpecialExam = async (req, res, next) => {
     optionalSubject, //array of subject Id
     subjectInfo, //array of subjectinfo
   } = req.body;
-  console.log(req.body);
+  const negativeVarks = req.body.negativeMarks;
+  console.log(req.body.negativeMarks);
   console.log(negativeMarks);
   if (!ObjectId.isValid(courseId)) {
     return res.status(404).json("Course Id is not valid.");
