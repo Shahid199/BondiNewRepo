a1fcd832c330c1427f944b260aa51865ec05b07c
Shahid199 <uiu.shahid.imran@gmail.com>
2023-09-27 13:29:20 +0600

    both updated backend

---
+++ model/SpRank.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const exams = require("./SpecialExam");
+const students = require("./Student");
+const Schema = mongoose.Schema;
+
+const specialRankSchema = new Schema(
+  {
+    examId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: exams,
+    },
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: students,
+    },
+    totalObtainedMarks: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    rank: {
+      type: Number,
+      required: true,
+      default: -1,
+    },
+  },
+  { timestamps: true } //createdAt,updatedAt auto genrate in the DB table.
+);
+
+module.exports = mongoose.model("SpRank", specialRankSchema);
