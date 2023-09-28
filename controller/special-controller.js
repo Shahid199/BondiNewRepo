7cec8fc0d1c031ecdcf392b70ad5ceedf52a6f9b
Shahid199 <uiu.shahid.imran@gmail.com>
2023-09-28 12:04:01 +0600

    both updated backend

--- controller/special-controller.js
+++ controller/special-controller.js
@@ -254,8 +254,20 @@ const showSpecialExamByIdStudent = async (req, res, next) => {
   } catch (err) {
     return res.status(500).json("Something went wrong.");
   }
+  let dataWritten = null;
+  try {
+    dataWritten = await SpecialExam.findOne({
+      $and: [{ examId: examId }],
+    });
+  } catch (err) {
+    return res.status(500).json("Something went wrong.");
+  }
+  let writtenObj = {};
+  writtenObj["totalWrittenMarks"] = dataWritten.totalMarksWritten;
+  writtenObj["writtenDuration"] = dataWritten.writtenDuration;
+  writtenObj["marksPerSub"] = dataWritten.totalMarksWritten / 4;
   if (data == null) return res.status(404).json("No data found.");
-  return res.status(200).json(data);
+  return res.status(200).json({ data, writteObj });
 };
 const showSpecialExamByCourse = async (req, res, next) => {
   let courseId = req.query.courseId;
