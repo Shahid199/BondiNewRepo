fd1059a62807df5cba4540ef66dad198fd45caff
Shahid199 <uiu.shahid.imran@gmail.com>
2023-10-01 17:26:59 +0600

    both updated backend

--- controller/special-controller.js
+++ controller/special-controller.js
@@ -2043,7 +2043,6 @@ const getStudentData = async (req, res, next) => {
     console.log(err);
     return res.status(500).json("Something went wrong.");
   }
-  console.log("dataex:", dataEx);
   questionData = dataEx.questionWritten;
   let indexValue = null;
   for (let i = 0; i < 4; i++) {
@@ -2053,6 +2052,7 @@ const getStudentData = async (req, res, next) => {
       break;
     }
   }
+  console.log("dataex:", dataEx[indexValue]);
   try {
     students = await TeacherVsSpecialExam.findOne({
       $and: [{ teacherId: teacherId }, { examId: examId }],
