e8d5e4c6dd84e302c46a5f70504e3f7aaa4e30d4
Shahid199 <uiu.shahid.imran@gmail.com>
2023-10-04 11:32:54 +0600

    new commit

--- controller/exam-controller.js
+++ controller/exam-controller.js
@@ -190,8 +190,8 @@ const updateExam = async (req, res, next) => {
       .json("exam Id or course Id or subject Id is not valid.");
   }
   console.log("CT", moment(new Date()));
-  console.log("ST", startTime);
-  console.log("ET", endTime);
+  console.log("ST", moment(startTime));
+  console.log("ET", moment(endTime));
 
   let saveExamUpd = {
     courseId: new mongoose.Types.ObjectId(courseId),
@@ -200,8 +200,8 @@ const updateExam = async (req, res, next) => {
     examType: Number(examType),
     examVariation: Number(examVariation),
     examFreeOrNot: JSON.parse(examFreeOrNot),
-    startTime: startTime,
-    endTime: endTime,
+    startTime: moment(startTime),
+    endTime: moment(endTime),
     duration: Number(duration),
     totalQuestionMcq: Number(totalQuestionMcq),
     marksPerMcq: Number(marksPerMcq),
