a8e311e6c72c60b776347dc0d4ffce246db1d092
Shahid199 <uiu.shahid.imran@gmail.com>
2023-10-04 23:03:59 +0600

    new commit

--- controller/student-controller.js
+++ controller/student-controller.js
@@ -928,7 +928,9 @@ const submitAnswer = async (req, res, next) => {
     finishedStatus: true,
     runningStatus: false,
     examEndTime: moment(submitTime).add(6, "h"),
-    duration: Number(submitTime - moment(timeStudent[0])) / (1000 * 60),
+    duration:
+      Number(moment(submitTime).add(6, "h") - moment(timeStudent[0])) /
+      (1000 * 60),
   };
   try {
     saveStudentExamEnd = await StudentMarksRank.findByIdAndUpdate(
