c79d85245ef660fd78617a980172b3183400f9e9
Shahid199 <uiu.shahid.imran@gmail.com>
2023-10-17 15:12:00 +0600

    new commit

--- controller/special-controller.js
+++ controller/special-controller.js
@@ -2855,7 +2855,7 @@ const getRecheckStudentData = async (req, res, next) => {
   for (let i = 0; i < checkStatus.length; i++) {
     let questionData = null;
     let indexValue = null;
-    console.log("checkStatus", checkStatus[i]);
+    console.log("checkStatus", checkStatus[i].questionWritten);
     for (let j = 0; j < 4; j++) {
       console.log("j", j);
       if (
