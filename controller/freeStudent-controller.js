40e409a48aff16d6f9f010bdb037f953c07829a4
Shahid199 <uiu.shahid.imran@gmail.com>
2023-12-27 02:34:37 +0600

    new commit

--- controller/freeStudent-controller.js
+++ controller/freeStudent-controller.js
@@ -1915,6 +1915,7 @@ const testApi = async (req, res, next) => {
         },
       });
   } catch (err) {
+    console.log(err);
     return res.status(500).json("Can't get exam info.");
   }
   return res.status(200).json(getExamData);
