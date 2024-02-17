55987e06edc63f498a9bb5382b7d40bc145fe1bf
Shahid199 <uiu.shahid.imran@gmail.com>
2024-02-17 15:13:25 +0600

    back to server code

--- controller/special-controller.js
+++ controller/special-controller.js
@@ -440,10 +440,11 @@ const studentSubmittedExamDetail = async (req, res, next) => {
     dataObject["subjectName"] = data.questionMcq[i].subjectId.name;
     dataObject["marksMcqPerSub"] =
       data.questionMcq[i].mcqMarksPerSub.toFixed(2);
-    if (!data.questionWritten[i].totalObtainedMarksWritten) {
-      dataObject["marksWrittenPerSub"] = dataObject["marksWrittenPerSub"] = 0;
+    if (!data.endTimeWritten == null) {
+      dataObject["marksWrittenPerSub"] = 0;
     } else {
-      data.questionWritten[i].totalObtainedMarksWritten.toFixed(2);
+      dataObject["marksWrittenPerSub"] =
+        data.questionWritten[i].totalObtainedMarksWritten.toFixed(2);
     }
     dataObject["totalMarksMcqPerSub"] = data.examId.totalMarksMcq / 4;
     dataObject["totalMarksWrittenPerSub"] = (
