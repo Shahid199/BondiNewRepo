f873de67dc9595bcb50fc28b2d403eee608027e5
Shahid199 <uiu.shahid.imran@gmail.com>
2024-04-01 16:53:21 +0600

    remove file remove one time

--- controller/removefile-controller.js
+++ controller/removefile-controller.js
@@ -58,7 +58,7 @@ const removeOneTime = async (req, res, next) => {
           if (data[i].questionWritten[j]) {
             let subjects = data[i].questionWritten[j].submittedScriptILink;
             console.log(subjects);
-            if (subjects.length > 0 || subjects) {
+            if (subjects.length > 0) {
               for (let k = 0; k < subjects.length; k++) {
                 for (let p = 0; p < k.length; p++) {
                   path.push(subjects[k][p]);
