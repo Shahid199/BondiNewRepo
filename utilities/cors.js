2082af5bb774cc2797e9b7d9a19d3ef73f410aec
unknown <uiu.afser@gmail.com>
2025-01-05 04:06:17 +0600

    new address added for cors

--- utilities/cors.js
+++ utilities/cors.js
@@ -1,28 +1,29 @@
-const cors = require("cors");
+const cors = require('cors');
 let options = {
   origin: [
-    "http://localhost:3001",
-    "http://localhost:3000",
-    "http://localhost:3002",
-    "https://admin.bpexam.live",
-    "https://bpexam.live",
-    "https://free.bpexam.live",
-    "https://www.bpexam.live",
+    'http://localhost:3001',
+    'http://localhost:3000',
+    'http://localhost:3002',
+    'https://admin.bpexam.live',
+    'https://bpexam.live',
+    'https://free.bpexam.live',
+    'https://www.bpexam.live',
   ],
-  "Access-Control-Allow-Origin": [
-    "http://localhost:3001",
-    "http://localhost:3000",
-    "http://localhost:3002",
-    "https://admin.bpexam.live",
-    "https://bpexam.live",
-    "https://free.bpexam.live",
-    "https://www.bpexam.live",
+  'Access-Control-Allow-Origin': [
+    'http://localhost:3001',
+    'http://localhost:3000',
+    'http://localhost:3002',
+    'https://admin.bpexam.live',
+    'https://bpexam.live',
+    'https://free.bpexam.live',
+    'https://www.bpexam.live',
+    'https://gpcmp.grameenphone.com/ecmapigw/webresources/ecmapigw.v2',
   ],
-  "Access-Control-Request-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-  "X-Requested-With": "XMLHttpRequest",
-  "X-Frame-Options": "SAMEORIGIN",
-  "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, Content-Length, X-Requested-With, X-Frame-Options",
+  'Access-Control-Request-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-Frame-Options': 'SAMEORIGIN',
+  'Access-Control-Allow-Headers':
+    'Content-Type, Authorization, Content-Length, X-Requested-With, X-Frame-Options',
   credentials: true,
 };
 
